cb3b7f44ddfe080c628326baf9a9e528bee990df
Tanner Griffin <tannermgriffin@gmail.com>
2020-04-28 19:19:55 -0500

    added stylesheet, added min/max length to length variable in script

--- assets/js/pwgenerator.js
+++ assets/js/pwgenerator.js
@@ -2,9 +2,11 @@ var length, lowerCase, upperCase, numeric, symbol, charArray, charset, password;
 
 //function for execution on button click
 document.getElementById('generate').onclick = function generate() {
-	length = prompt('How many characters?', '8');
+	length = prompt('How many characters?', '8 min; 128 max');
 	//convert length prompt to number
-	len = parseInt(length);
+    len = parseInt(length);
+    // set min/max length to 8-128
+    len = Math.min(128, Math.max(8, len));
 	lowerCase = confirm('Include lowercase characters?');
 	upperCase = confirm('Include uppercase characters?');
 	numeric = confirm('Include numeric characters?');
@@ -32,14 +34,14 @@ document.getElementById('generate').onclick = function generate() {
         charset += charArray[3];
     }
     //if no character types selected, return numeric password and alert
-    if (charset == '') {
+    if (charset === '') {
         charset += charArray[2];
         alert("Requires one character type. Numbers were selected by default.");
     }
 
 	for (var i = 0, n = charset.length; i < len; ++i) {
 		password += charset.charAt(Math.floor(Math.random() * n));
-	}
+    }
 	//print new password
-	document.getElementById('pass').innerHTML = password;
+    document.getElementById('pass').innerHTML = password;
 };
